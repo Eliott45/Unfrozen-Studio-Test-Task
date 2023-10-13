c410ddb16cf1ae1dd6e0d8a321d7d4a8bcc62377
Maksim <eliott_nu@mail.ru>
2023-10-13 17:24:05 +0900

    Create view and view controller for pre-mission

---
+++ Test-Task/Assets/Scripts/UI/Controllers/PreMissionViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Missions.Data;
+using UI.Views;
+
+namespace UI.Controllers
+{
+    public class PreMissionViewController : IDisposable
+    {
+        public event Action OnStartMission;
+        
+        private readonly PreMissionView _view;
+
+        private MissionInfo _missionInfo;
+        
+        public PreMissionViewController(PreMissionView view)
+        {
+            _view = view ? view : throw new NullReferenceException(nameof(PreMissionView));
+        }
+
+        public void Initialize(MissionInfo missionInfo)
+        {
+            _missionInfo = missionInfo ?? throw new NullReferenceException(nameof(MissionInfo));
+
+            _view.SetName(_missionInfo.Name);
+            _view.SetDescription(_missionInfo.Description);
+            _view.SetPreviewSprite(_missionInfo.Preview);
+            _view.OnStartButton += OnStartMission;
+        }
+
+        public void Dispose()
+        {
+            _view.OnStartButton -= OnStartMission;
+        }
+
+        public void ShowView()
+        {
+            _view.gameObject.SetActive(true);
+        }
+
+        public void HideView()
+        {
+            _view.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Test-Task/Assets/Scripts/UI/Views/PreMissionView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Views
+{
+    public class PreMissionView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private TextMeshProUGUI _descriptionText;
+        [SerializeField] private Image _previewImage;
+        [SerializeField] private Button _startButton;
+
+        public event Action OnStartButton;
+
+        private void OnEnable()
+        {
+            _startButton.Subscribe(() => OnStartButton?.Invoke());
+        }
+
+        private void OnDisable()
+        {
+            _startButton.UnsubscribeAll();
+        }
+
+        public void SetName(string missionName)
+        {
+            _nameText.SetText(missionName);
+        }
+
+        public void SetDescription(string description)
+        {
+            _descriptionText.SetText(description);
+        }
+
+        public void SetPreviewSprite(Sprite preview)
+        {
+            _previewImage.sprite = preview;
+        }
+
+        public void UpdateButtonInteractable(bool interactable)
+        {
+            _startButton.interactable = interactable;
+        }
+    }
+}
